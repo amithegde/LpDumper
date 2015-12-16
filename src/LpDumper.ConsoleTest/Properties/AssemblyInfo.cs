fd5c8f1b24d3c86f4df48ee130af2df9bfcf448f
Amit hegde <amits.hegde@gmail.com>
2015-12-15 19:16:52 -0800

    Assemblyinfo updated

--- src/LpDumper.ConsoleTest/Properties/AssemblyInfo.cs
+++ src/LpDumper.ConsoleTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LpVisualizer.Test")]
+[assembly: AssemblyTitle("LpDumper.ConsoleTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LpVisualizer.Test")]
+[assembly: AssemblyProduct("LpDumper.ConsoleTest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
